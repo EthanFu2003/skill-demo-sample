6a9eed0c739f146cb87afa2ae9141525a4998c1c
EthanFu2003 <xif001@ucsd.edu>
2022-05-01 13:24:13 -0700

    Added a method

---
+++ SkillDemoSampleTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoSampleTest {
+
+    @Test
+    public void multiplication() {
+        assertEquals(2, SkillDemoSample.multiply(1, 1));
+    }
+
+}
